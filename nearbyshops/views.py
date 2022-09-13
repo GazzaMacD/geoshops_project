f9ce385bacb4d93078fdd7eafab4d20612559bd8
Gareth Macdonald <macdonald.gareth@gmail.com>
2022-09-13 18:14:55 +0200

    Add view, urls and templates, data for home

--- nearbyshops/views.py
+++ nearbyshops/views.py
@@ -1,3 +1,21 @@
+from django.contrib.gis.db.models.functions import Distance
+from django.contrib.gis.geos import Point
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from nearbyshops.models import Shop
+
+latitude = 39.290440
+longitude = -76.612330
+
+user_location = Point(longitude, latitude, srid=4326)
+
+
+class Home(generic.ListView):
+    model = Shop
+    context_object_name = "shops"
+    queryset = Shop.objects.annotate(
+        distance=Distance("location", user_location)
+    ).order_by("distance")[0:6]
+    print(queryset)
+    template_name = "index.html"
